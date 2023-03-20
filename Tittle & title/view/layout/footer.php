401b1db55eb02e5dde0aca6481264050feaced72
anthanh-github <84690458+anthanh-github@users.noreply.github.com>
2023-03-20 12:52:41 +0700

    Update footer.php

--- Tittle & title/view/layout/footer.php
+++ Tittle & title/view/layout/footer.php
@@ -1,30 +1,4 @@
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Delete</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                Do you want to delete it?
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                <a href="#" class="btn btn-primary">Confirm</a>
-            </div>
-        </div>
-    </div>
-</div>
-<script src="public/vendor/jquery-3.5.1.min.js"></script>
-<script src="public/vendor/popper.min.js"></script>
-<script src="public/vendor/bootstrap-4.5.3-dist/js/bootstrap.min.js"></script>
-<script src="public/vendor/jquery-validation/dist/jquery.validate.min.js"></script>
-<script src="public/js/script.js"></script>
-</div>
+
 </body>
 
 </html>
