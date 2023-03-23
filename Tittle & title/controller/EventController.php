e64ff2a221be6dc1e18fd0383c24c87585cd2873
anthanh-github <84690458+anthanh-github@users.noreply.github.com>
2023-03-23 14:48:57 +0700

    Update EventController.php

--- Tittle & title/controller/EventController.php
+++ Tittle & title/controller/EventController.php
@@ -4,10 +4,23 @@ class EventController
 
     function index()
     {
+        $productRepository = new ProductRepository();
+        $products = $productRepository->getAll();
+
+        $conds = [];
+        $sorts = ['featured' => 'DESC'];
+        $featuredProducts = $productRepository->getBy($conds, $sorts, 1, 4);
+
+        $sorts = ['created_date' => 'DESC'];
+        $latestProducts = $productRepository->getBy($conds, $sorts, 1, 4);
         require 'view/event/index.php';
     }
     function detail()
     {
+        $id = $_GET['id'];
+        $productRepository = new ProductRepository();
+        $product = $productRepository->find($id);
+
         require 'view/event/detail.php';
     }
 }
